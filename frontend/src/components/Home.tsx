1998ec8b4d64c520f443c89ac34a50132c14e489
CCWebb14 <CCWebb14@gmail.com>
2025-03-30 06:47:55 -0700

    fixed api key, map rerender on navigate

--- frontend/src/components/Home.tsx
+++ frontend/src/components/Home.tsx
@@ -1,29 +1,21 @@
 // src/components/Dashboard.tsx
-import React, { useState } from "react";
-import { GoogleMap, LoadScript, Polygon } from "@react-google-maps/api";
+import React from "react";
+import { GoogleMap, LoadScriptNext, Polygon } from "@react-google-maps/api";
 import data from './data/local-area-boundary.json'
-
-import WebPImage from "../assets/img/Vancouver.webp";
+const apiKey = import.meta.env.VITE_GMAPS_API_KEY
 
 const Dashboard: React.FC = () => {
-  const [mapKey, setMapKey] = useState(0);
-
-  const refreshMap = () => {
-    setMapKey((prevKey) => prevKey + 1); // Increment the key to force re-render
-  };
   
   const mapContainerStyle = {
     width: "80%",
     height: "500px",
     margin: "auto",
   };
-
   
   const center = {
-    lat: 49.2827, // Latitude for Vancouver
-    lng: -123.1207, // Longitude for Vancouver
+    lat: 49.25, // Latitude for Vancouver
+    lng: -123.11, // Longitude for Vancouver
   };
- 
 
     const postalCodePolygons = data.map((area, index) => ({
       postalCode: area.name,
@@ -32,28 +24,35 @@ const Dashboard: React.FC = () => {
     }));
 
     return (
-      <LoadScript googleMapsApiKey="AIzaSyCZIW06LMeM9IsMGSLVuYDiYTkXvPKzxs0">
-        <GoogleMap
-          mapContainerStyle={mapContainerStyle}
-          center={center}
-          zoom={13}
-        >
-          {postalCodePolygons.map((polygon, index) => (
-            
-            <Polygon
-              key={index}
-              paths={polygon.paths}
-              options={{
-                fillColor: polygon.color,
-                fillOpacity: 0.4,
-                strokeColor: polygon.color,
-                strokeOpacity: 0.8,
-                strokeWeight: 2,
-              }}
-            />
-          ))}
-        </GoogleMap>
-      </LoadScript>
+        <>
+        {apiKey ? (
+          <LoadScriptNext googleMapsApiKey={apiKey}>
+            <GoogleMap
+              mapContainerStyle={mapContainerStyle}
+              center={center}
+              zoom={12}
+            >
+              {postalCodePolygons.map((polygon, index) => (
+                <Polygon
+                  key={index}
+                  paths={polygon.paths}
+                  options={{
+                    fillColor: polygon.color,
+                    fillOpacity: 0.4,
+                    strokeColor: polygon.color,
+                    strokeOpacity: 0.8,
+                    strokeWeight: 2,
+                  }}
+                />
+              ))}
+            </GoogleMap>
+          </LoadScriptNext>
+        ) : (
+          <div className="error-message">
+            Google Maps API key is missing. Please check your environment variables.
+          </div>
+        )}
+      </>
     );
 };
 
